9bb9739c22b95b9c6e3499729d51cf313515152e
Hamna-Tanveer <hamnatanveer345@gmail.com>
2025-08-29 22:07:57 +0500

    Create logiin page UI for Admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import Login from "./pages/Login";
 
 function App() {
-  return <div className="text-3xl text-red-400">App</div>;
+  return (
+    <div>
+      <Login />
+    </div>
+  );
 }
 
 export default App;
