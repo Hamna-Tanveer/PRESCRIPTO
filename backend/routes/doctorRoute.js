a9949ed09a1b538f585ec7771d62a3436eef2a0a
Hamna-Tanveer <hamnatanveer345@gmail.com>
2025-08-30 05:05:32 +0500

    create getDoctor api end point for user view

---
+++ backend/routes/doctorRoute.js
@@ -0,0 +1,7 @@
+import express, { Router } from "express";
+import { doctorList } from "../controller/doctorController.js";
+
+const doctorRouter = express.Router();
+
+doctorRouter.get("/list", doctorList);
+export default doctorRouter;
--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,13 +1,35 @@
-import { createContext } from "react";
-import { doctors } from "../assets/assets";
+import { createContext, useEffect, useState } from "react";
+
+import axios from "axios";
+import { toast } from "react-toastify";
 export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
+  const [doctors, setDoctors] = useState([]);
   const currencySymbol = "$";
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+  const getAllDoctors = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + "/api/doctor/list");
+      if (data.success) {
+        setDoctors(data.doctors);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
   const value = {
     currencySymbol,
     doctors,
   };
+
+  useEffect(() => {
+    getAllDoctors();
+  }, []);
   return (
     <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
   );
