0ff4bf57a0c89fb3a8e0a649912efbbfcc2aaa95
Hamna-Tanveer <hamnatanveer345@gmail.com>
2025-09-03 02:44:15 +0500

    create doctor routes and sidebar for doctor panel

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -11,6 +11,9 @@ import AllAppointments from "./pages/admin/AllAppointments";
 import AddDoctor from "./pages/admin/AddDoctor";
 import DoctorsList from "./pages/admin/DoctorsList";
 import { DoctorContext } from "./context/DoctorContext";
+import DoctorDashboard from "./pages/doctor/DoctorDashboard";
+import DoctorAppointments from "./pages/doctor/DoctorAppointments";
+import DoctorProfile from "./pages/doctor/DoctorProfile";
 function App() {
   const { aToken } = useContext(AdminContext);
   const { dToken } = useContext(DoctorContext);
@@ -21,11 +24,17 @@ function App() {
       <div className="flex items-start">
         <Sidebar />
         <Routes>
+          {/* -----Admin Routes------- */}
           <Route path="/" element={<></>} />
           <Route path="/admin-dashboard" element={<Dashboard />} />
           <Route path="/all-appointments" element={<AllAppointments />} />
           <Route path="/add-doctor" element={<AddDoctor />} />
           <Route path="/doctor-list" element={<DoctorsList />} />
+
+          {/* -----Doctor Routes------- */}
+          <Route path="/doctor-dashboard" element={<DoctorDashboard />} />
+          <Route path="/doctor-appointments" element={<DoctorAppointments />} />
+          <Route path="/doctor-profile" element={<DoctorProfile />} />
         </Routes>
       </div>
     </div>
--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -2,8 +2,10 @@ import { useContext } from "react";
 import { AdminContext } from "../context/AdminContext";
 import { NavLink } from "react-router-dom";
 import { assets } from "../assets/assets";
+import { DoctorContext } from "../context/DoctorContext";
 function Sidebar() {
   const { aToken } = useContext(AdminContext);
+  const { dToken } = useContext(DoctorContext);
   return (
     <div className="min-h-screen bg-white border-r">
       {aToken && (
@@ -51,6 +53,45 @@ function Sidebar() {
           >
             <img src={assets.people_icon} alt="loading..." />
             <p>Doctors List</p>
+          </NavLink>
+        </ul>
+      )}
+
+      {dToken && (
+        <ul className="text-[#515151] mt-5">
+          <NavLink
+            className={({ isActive }) =>
+              `flex items-center gap-3 px-3 py-3.5 md:px-9 md:min-w-72 cursor-pointer ${
+                isActive ? "bg-[#F2F3FF] border-r-4 border-primary" : ""
+              }`
+            }
+            to={"/doctor-dashboard"}
+          >
+            <img src={assets.home_icon} alt="loading..." />
+            <p>Dashboard</p>
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              `flex items-center gap-3 px-3 py-3.5 md:px-9 md:min-w-72 cursor-pointer ${
+                isActive ? "bg-[#F2F3FF] border-r-4 border-primary" : ""
+              }`
+            }
+            to={"/doctor-appointments"}
+          >
+            <img src={assets.appointment_icon} alt="loading..." />
+            <p>Appointments</p>
+          </NavLink>
+
+          <NavLink
+            className={({ isActive }) =>
+              `flex items-center gap-3 px-3 py-3.5 md:px-9 md:min-w-72 cursor-pointer ${
+                isActive ? "bg-[#F2F3FF] border-r-4 border-primary" : ""
+              }`
+            }
+            to={"/doctor-profile"}
+          >
+            <img src={assets.people_icon} alt="loading..." />
+            <p>Profile</p>
           </NavLink>
         </ul>
       )}
