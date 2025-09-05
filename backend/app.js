da9fb47be23b9e50fc591bf7e0ed2d206e1b70c0
Hamna-Tanveer <hamnatanveer345@gmail.com>
2025-09-05 07:25:37 +0500

    cors update

--- backend/app.js
+++ backend/app.js
@@ -17,6 +17,7 @@ app.use(
       "https://prescripto-lxu3.vercel.app",
       "http://localhost:5174",
       "http://127.0.0.1:5174",
+      "http://localhost:5173",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
